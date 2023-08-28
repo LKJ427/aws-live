6fd7856bb988cf0fc2e30168b092e70cd9805936
Lim Khai Jun <86950025+LKJ427@users.noreply.github.com>
2023-08-28 11:49:53 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-2.czxfkdwszg0m.us-east-1.rds.amazonaws.com"
+customuser = "lkj"
+custompass = "asdf1234!"
+customdb = "employee"
+custombucket = "limkhaijun-employee"
+customregion = "us-east-1"
+
